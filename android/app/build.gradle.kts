7d875b5ad7b93e623f643662c29b259db816012e
arnim123 <goyalarnim123@gmail.com>
2026-03-09 15:49:52 +0530

    changes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,9 +1,28 @@
+import java.util.Properties
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
+
+val localProps = Properties().also { p ->
+    val f = rootProject.file("local.properties")
+    if (f.exists()) {
+        f.inputStream().use { p.load(it) }
+    }
+}
+
+val flutterVersionCode = (localProps.getProperty("flutter.versionCode") ?: "1").toInt()
+val flutterVersionName = localProps.getProperty("flutter.versionName") ?: "1.0"
+
+val keystoreProps = Properties().also { p ->
+    val f = rootProject.file("key.properties")
+    if (f.exists()) {
+        f.inputStream().use { p.load(it) }
+    }
+}
+
 
 android {
     namespace = "com.bsky.neetcounsellingapp"
@@ -30,15 +49,36 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            val storeFilePath = keystoreProps.getProperty("storeFile")?.takeIf { it.trim().isNotEmpty() }
+            if (storeFilePath != null) {
+                storeFile = file(storeFilePath)
+                storePassword = keystoreProps.getProperty("storePassword")
+                keyAlias = keystoreProps.getProperty("keyAlias")
+                keyPassword = keystoreProps.getProperty("keyPassword")
+            }
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = true
+            isShrinkResources = true
+            // attach signing if it exists
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                file("proguard-rules.pro")
+            )
+            signingConfigs.findByName("release")?.let { signingConfig = it }
         }
     }
 }
 
 flutter {
     source = "../.."
+}
+
+dependencies{
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
 }
